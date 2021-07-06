92173ca6b8591c4b268403092fd710916a52987e
LeoPalacioM <lpalaciom@hotmail.com>
2021-07-05 23:14:03 -0500

    iniciando

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => (
+  <Page>
+    <Heading>Shopify app with Node, React and Kike 🎉</Heading>
+  </Page>
+);
+
+export default Index;
